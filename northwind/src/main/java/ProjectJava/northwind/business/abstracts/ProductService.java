ac6b9a064e504d12ccb737f26abed296ece79f05
berkay <berkay.kurkcu@gmail.com>
2021-12-27 14:31:02 +0300

    core layer added and uptaded, result system generated also post methods added to product controller.

--- northwind/src/main/java/ProjectJava/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/ProjectJava/northwind/business/abstracts/ProductService.java
@@ -2,9 +2,12 @@ package ProjectJava.northwind.business.abstracts;
 
 import java.util.List;
 
+import ProjectJava.northwind.core.utilities.results.DataResult;
+import ProjectJava.northwind.core.utilities.results.Result;
 import ProjectJava.northwind.entitites.concretes.Product;
 
 public interface ProductService {
-	List<Product> getAll();
+	DataResult<List<Product>> getAll();
+	Result add(Product product);
 
 }
