d079e1f93cbe9751fb37b7452ed22e698d5f6ec1
berkay <berkay.kurkcu@gmail.com>
2021-12-28 21:37:37 +0300

    Category class added.Coded about JpaRepository.I made some changes accordingly.

--- northwind/src/main/java/ProjectJava/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/ProjectJava/northwind/business/abstracts/ProductService.java
@@ -2,12 +2,31 @@ package ProjectJava.northwind.business.abstracts;
 
 import java.util.List;
 
+
+
 import ProjectJava.northwind.core.utilities.results.DataResult;
 import ProjectJava.northwind.core.utilities.results.Result;
 import ProjectJava.northwind.entitites.concretes.Product;
 
 public interface ProductService {
 	DataResult<List<Product>> getAll();
+	DataResult<List<Product>> getAll(int pageNo,int pageSize);
+	DataResult<List<Product>> getAllSorted();
 	Result add(Product product);
+	
+	DataResult<Product> getByProductName(String productName);
+	
+	DataResult<Product> getByProductNameAndCategoryId(String productName,int categoryId);
+	
+	DataResult<List<Product>> getByProductNameOrCategoryId(String productName,int categoryId);
+
+	DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);
+	
+	DataResult<List<Product>> getByProductNameContains(String productName);
+	
+	DataResult<List<Product>> getByProductNameStartsWith(String productName);
+	
+	DataResult<List<Product>> getByNameAndCategory(String productName,int categoryId);
+
 
 }
